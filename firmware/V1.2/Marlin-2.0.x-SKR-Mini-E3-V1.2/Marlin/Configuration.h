67946c1378a94c22124aa735802bd051ff4b7a6c
Alberto Martín <adm.martin.tr@gmail.com>
2021-04-01 10:56:56 +0200

    Adjust the stepper max current in order to avoid the overheating; following the next criteria:
    I purchased the BigTreeTech SKR Mini E3 v1.2 board for my Ender 3. After replacing the board I found that my motors were getting very hot. I thought it has something to do with the way that I sliced the print. After a lot of Google searching, multiple print failures caused my skipping due to high motor temperatures and a burned up x-axis motor, I finally pieced together the root cause.
    
    The factory board was set to 580mA for x, y & z. When I installed the SKR Mini and updated the firmware, I found that the steppers were set to 580mA - perfect, right? nope that was the root cause of overheating stepper motors.
    
    The factory board uses the A4982 stepper drivers and current set on those is the max-current. The SKR Mini uses the TMC2209 stepper drivers. When setting the current on those drivers you DO NOT set the max-current but rather the RMS-current. How do you calculate the RMS-current? RMS-current = max-current / 1.414.
    
    In order to match the max-current from the factory board, 580mA(max-current) / 1.414 = 410mA(RMS-current)
    In the firmware, the SKR Mini x, y & z current needs to be set to 410mA and not 580mA.
    
    So why were the motors running hot and eventually burned up? Because they were set to 580mA(RMS-current) which results in 820mA max-current. Since the motors are rated at 840mA max-current, there was too much current going to the motors.
    
    I hope this helps someone out there.

--- firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(StopinWonder, Ender-3 PRO)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "StopinWonder" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Toothless r.1"
+#define CUSTOM_MACHINE_NAME "Toothless r.2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
